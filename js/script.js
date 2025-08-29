67738abc53d221130856c599092ae234926c9614
Behruz <ibehruzzaripov@gmail.com>
2025-08-29 15:36:40 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// const storeName = "beeki";
+
+// const storeDescription = {
+//   budget: 1000,
+//   employees: ["Alex", "John", "David"],
+//   products: {
+//     soup: 20,
+//     water: 5,
+//   },
+//   open: true,
+// };
+
+"use strict";
+
+const user = "Behruz";
+
+alert(`Salom, ${User}`);
